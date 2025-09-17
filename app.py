528fc77a6df30eb9c4896b551fca64ae1facf245
Laura Hainari <laura@Laura-MacBook-Air.local>
2025-09-17 12:04:29 +0300

    Edit the error handling in login and sign up

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+"""This is an application used to create and log "Workouts of the Day" (WOD) in a crossfit trainning center"""
+
 import sqlite3
 from flask import Flask
 from flask import redirect, render_template, request, session
@@ -11,44 +13,51 @@ app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    if "user_id" not in session: #if the user is not logged in... we shall go to login
+    """Front-page view"""
+    if "user_id" not in session:
         return redirect("/login")
     workout_list = workouts.list_workouts(user_id=session["user_id"])
     return render_template("index.html", workouts=workout_list)
 
 @app.route("/workout/<int:workout_id>")
 def show_workout(workout_id):
+    """list the workouts"""
     workout=workouts.list_workout(workout_id)
     return render_template("show_workout.html", workout=workout)
 
 @app.route("/new_workout")
 def new_workout():
-  return render_template("new_workout.html")
+    """register a new workout log"""
+    return render_template("new_workout.html")
 
 @app.route("/create_workout", methods=["POST"])
 def create_workout():
+    """create workout"""
     workout_date=request.form["workout_date"]
     warmup_description=request.form["warmup_description"]
     wod_description=request.form["wod_description"]
     extras_description=request.form["extras_description"]
     user_id=session["user_id"]
 
-    workouts.add_workout(workout_date, warmup_description, wod_description, extras_description, user_id)    
+    workouts.add_workout(workout_date,warmup_description,wod_description,extras_description,user_id)
 
     return redirect("/") #for now let's go to the frontpage, but figure out something better later
 
 @app.route("/register")
 def register():
+    """register a new user"""
     return render_template("register.html")
 
 @app.route("/create", methods=["POST"])
 def create():
+    """create a new user profile"""
     username = request.form["username"]
     password1 = request.form["password1"]
     password2 = request.form["password2"]
 
     if password1 != password2:
-        return "ERROR: The passwords must be the same" #this should be re-routed to a sign_in/sign_up page with a note
+        return render_template("register.html", error="ERROR: The passwords must be the same")
+
 
     is_coach = 1 if request.form.get("is_coach") else 0
     password_hash = generate_password_hash(password1)
@@ -57,34 +66,40 @@ def create():
         sql = "INSERT INTO users (username, password_hash, is_coach) VALUES (?, ?, ?)"
         db.execute(sql, [username, password_hash, is_coach])
     except sqlite3.IntegrityError:
-        return "ERROR: The username is already in use" #this should be re-routed to a sign_in/sign_up page with note
+        return render_template("register.html", error="ERROR: The username is already in use")
 
-    return redirect("/login") #return to login
+    return redirect("/login")
 
 @app.route("/login", methods=["GET","POST"])
 def login():
-    error=None
+    """log in to the application"""
     if request.method == "GET":
-      return render_template("login.html")
+        return render_template("login.html")
 
     if request.method=="POST":
-      username = request.form["username"]
-      password = request.form["password"]
+        username = request.form["username"]
+        password = request.form["password"]
 
-      sql = "SELECT id, password_hash FROM users WHERE username = ?"
-      result = db.query(sql, [username])[0]
-      user_id = result["id"]
-      password_hash = result["password_hash"]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])
 
-      if check_password_hash(password_hash, password):
-          session["user_id"]= user_id
-          session["username"] = username
-          return redirect("/")
-      else:
-          return "ERROR: wrong username or password"
+        if not result:
+            return render_template("login.html", error="Wrong username or password")
+
+        user=result[0]
+        user_id = user["id"]
+        password_hash = user["password_hash"]
+
+        if check_password_hash(password_hash, password):
+            session["user_id"]= user_id
+            session["username"] = username
+            return redirect("/")
+
+        return render_template("login.html", error="Wrong username or password")
 
 @app.route("/logout")
 def logout():
+    """logout from the application and delete the session"""
     del session["user_id"]
     del session["username"]
     return redirect("/")
--- db.py
+++ db.py
@@ -1,24 +1,31 @@
+"""Database handling"""
 import sqlite3
 from flask import g
 
 def get_connection():
-    con = sqlite3.connect("database.db")
+    con = sqlite3.connect("database.db", timeout=5)
     con.execute("PRAGMA foreign_keys = ON")
     con.row_factory = sqlite3.Row
     return con
 
 def execute(sql, params=[]):
     con = get_connection()
-    result = con.execute(sql, params)
-    con.commit()
-    g.last_insert_id = result.lastrowid
-    con.close()
+    try:
+        result = con.execute(sql, params)
+        con.commit()
+        g.last_insert_id = result.lastrowid
+        return result
+    finally:
+        con.close()
 
 def last_insert_id():
-    return g.last_insert_id    
-    
+    return g.last_insert_id
+
 def query(sql, params=[]):
+    """Execute the read operation"""
     con = get_connection()
-    result = con.execute(sql, params).fetchall()
-    con.close()
-    return result
+    try:
+        result = con.execute(sql, params).fetchall()
+        return result
+    finally:
+        con.close()
